8be765b9f10d585d9c2aaa2f70c79c1658f22c40
srp.jv <julien.vionb@gmail.com>
2020-08-31 04:50:50 +0200

    modified:   scraping/chrome.py

--- code/scraping/chrome.py
+++ code/scraping/chrome.py
@@ -1,21 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 
 from bs4 import BeautifulSoup
-import urllib.request
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
-class scraping():
-    def __init__(self):
-        self.url = "https://airtable.com/shrRXLXmGwGPdOaLF/tblWjJI9H9EXyLPPF?backgroundColor=pink&layout=card&viewControls=on"
-
-    def loading(self):
-        self.driver.get(self.url)
+year = []
+driver = webdriver.Chrome(executable_path=r'C:\Users\vion1\Downloads\chromedriver_win32\\chromedriver.exe')
+driver.get('https://airtable.com/shrRXLXmGwGPdOaLF/tblWjJI9H9EXyLPPF?backgroundColor=pink&layout=card&viewControls=on')
 
 
+soup = BeautifulSoup(driver.page_source, "lxml")
+
+while True:
+    for link in soup.find_all(class_ = "flex-auto p1"): #Ou draggableRecord rowCardContainer animate
+        year = link.text
 
 
-variable = scraping()
-print(variable)
+#element = driver.find_element_by_class_name("draggableRecord rowCardContainer animate")
+#driver.execute_script("arguments[0].scrollIntoView(true);", element)
+driver.close()
+
+
